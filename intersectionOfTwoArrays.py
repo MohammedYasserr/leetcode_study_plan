2e64aa33b84dc3261bdea13b30ece242de80e061
MohammedYasserr <bmbmyasser@gmail.com>
2023-05-14 19:56:27 +0300

    Solving the problem using AND Bitwise operation

---
+++ intersectionOfTwoArrays.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def intersection(nums1: List[int] , nums2: List[int]) -> List[int]:
+    # Checking the Edge cases when one of the two input arrays are empty
+    return set(nums1) & set(nums2)
+
+
+print(intersection([1, 2, 3, 4], [2, 3])) 
+
+"""
+complexity analysis of using AND Bitwise 
+
+
+"""
