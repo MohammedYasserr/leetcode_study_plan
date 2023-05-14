741ca5750e43246ecf0156aaa921af4d04ba05a4
MohammedYasserr <bmbmyasser@gmail.com>
2023-05-14 20:40:07 +0300

    Fixing the unittests

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -1,9 +1,12 @@
 import unittest
-import intersection_of_two_arrays 
+from Intersection_of_two_arrays import intersection_of_two_arrays
 
 class intersection_of_two_arrays_test(unittest.TestCase):
 
-    def test_intersection_of_two_arrays(self):
+    def TestIntersection(self):
        assert intersection_of_two_arrays([1,2,3,4] , [2,3,4]) == [2,3,4]
        print("Did I manage to pass the test cases")
 
+
+if __name__ == '__main__':
+    unittest.main()
