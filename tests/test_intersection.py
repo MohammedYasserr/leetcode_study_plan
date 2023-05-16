8d570d1aed0378bb1ea8c0687990a92a336cc7fd
MohammedYasserr <bmbmyasser@gmail.com>
2023-05-16 04:37:52 +0300

    adding the sys.path to be able to import intersection function

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -1,15 +1,20 @@
 import unittest
-from Intersection_of_two_arrays import intersection_of_two_arrays
+import sys
 
-class intersection_of_two_arrays_test(unittest.TestCase):
+# Importing the file containg the directory to the environment OS
+sys.path.append('E:/Current/Projects/ds_workplace/Intersection_of_two_arrays')
 
-    def TestIntersection(self):
+from intersection_of_two_arrays import intersection
+
+class TestIntersection(unittest.TestCase):
+
+    def test_intersection(self):
+        """
+        This Test case shows that there is intersection between the two input arrays
+        """
         nums1 = [1,2,3,4]
         nums2 = [2,3]
-        result = intersection_of_two_arrays(nums1,nums2)
+        result = intersection(nums1,nums2)
         self.assertEqual(result, [2,3])
         print("Test Passed ! ")
 
-
-if __name__ == '__main__':
-    unittest.main()
