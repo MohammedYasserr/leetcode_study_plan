5e661084c163262f3b8406bb25720f2e5acc80c6
MohammedYasserr <bmbmyasser@gmail.com>
2024-06-10 08:49:55 +0200

    solving the problem of relative order of the sequence and subsequence

--- Is_subsequence/is_subsequence.py
+++ Is_subsequence/is_subsequence.py
@@ -1,33 +1,45 @@
 # Solving is subsequence - leetcode 
 
 
-def isSubsequence(s:str,t:str) -> bool :
+# def isSubsequence(s:str,t:str) -> bool :
      
         
+#     """
+#     Checking whether the string s is a subsequence of string t
+#     """
+        
+#     if len(s) > len(t):
+#         return False
+
+#     # Loop over the s sting
+#     for i in range(0,len(s)):
+        
+#         try:
+#             index = t.index(s[i])
+#         except ValueError:
+#             return False
+#             # Update the t string 
+#         t = t[index+1:]
+            
+#         return True
+    
+
+# print(isSubsequence("abc","fxacb")) #True
+# print(isSubsequence("abc","afcb"))
+# print(isSubsequence("rt" , "xbc")) #False
+# # print(isSubsequence("","etjh"))
+
+            
+def isSubsequence(s:str,t:str):
+    
     """
     Checking whether the string s is a subsequence of string t
     """
-        
-    if len(s) > len(t):
-        return False
-
-    # Loop over the s sting
-    for i in range(0,len(s)):
-        
-        try:
-            index = t.index(s[i])
-        except ValueError:
-            return False
-            # Update the t string 
-        t = t[index+1:]
-            
-        return True
+    remainder_of_t = iter(t)
     
+    for char in s : 
+        if char not in remainder_of_t:
+            return False
+    return True
 
-print(isSubsequence("abc","fxacb")) #True
-print(isSubsequence("abc","afcb"))
-print(isSubsequence("rt" , "xbc")) #False
-# print(isSubsequence("","etjh"))
-
-            
-        
+print(isSubsequence("abc","fxabc")) #True
